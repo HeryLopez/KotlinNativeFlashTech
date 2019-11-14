c4c333feffc0799bf2c37a7870ed3f7d4f3cb147
Hery Lopez <hlopez@sqli.com>
2019-11-14 17:05:43 +0100

    Kotlin native android and ios

---
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.jetbrains.handson.mpp.mobile
+
+import com.jetbrains.handson.mpp.mobile.createApplicationScreenMessage
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
+    }
+}
